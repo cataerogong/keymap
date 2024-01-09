1ff93ce4b41e77eb61a7434ac4be0d7180e6d2b4
CataeroGong <cataerogong@hotmail.com>
2024-01-09 15:06:23 +0800

    v2.0.0

--- exe/keymap.py
+++ exe/keymap.py
@@ -7,11 +7,11 @@ class KeymapApp(WebWinApp):
         self.argparser.del_argument('--mainpage')
         self.argparser.del_argument('--del-js')
         self.argparser.del_argument('--run-js')
-        self.argparser.add_argument('-P', metavar='profile', help='键谱配置名')
+        self.argparser.add_argument('-P', metavar='profile', help='键谱名')
         self.argparser.add_argument('-L', choices=['0','1','2','3','4'], help='键盘层')
-        self.argparser.add_argument('-S', choices=['1','2','3','4'], help='键盘大小（60%%,80%%,100%%,扩展）')
-        self.argparser.add_argument('-V', choices=['kb', 'list'], help='界面（键图，键表）')
-        self.argparser.add_argument('-H', choices=['true', 'false'], help='键帽提示')
+        self.argparser.add_argument('-V', choices=['k', 'l', 'j'], help='视图（键图/键表/数据）')
+        self.argparser.add_argument('-S', choices=['1','2','3','4'], help='键盘大小（60%%/80%%/100%%/扩展）')
+        self.argparser.add_argument('-H', choices=['0', '1'], help='键帽提示（关闭/打开）')
 
 app = KeymapApp()
 app.args.webroot = app.PROG_DIR
@@ -21,15 +21,14 @@ js = []
 if (app.args.P):
     js.append(f'await UI.load("{app.args.P}");')
 if (app.args.L is not None):
-    js.append(f'Keymap.Config.layer={app.args.L};')
-if (app.args.S):
-    js.append(f'Keymap.Config.kbsize={app.args.S};')
+    js.append(f'Keymap.CurLayer={app.args.L};')
 if (app.args.V):
     js.append(f'Keymap.Config.view="{app.args.V.lower()}";')
+if (app.args.S):
+    js.append(f'Keymap.Config.kbsize={app.args.S};')
 if (app.args.H):
-    js.append(f'Keymap.Config.kbhint={app.args.H.lower()};')
+    js.append(f'Keymap.Config.kbhint={"false" if (app.args.H.lower()=="0") else "true"};')
 if len(js):
     run_js = f'''(async () => {{ {"".join(js)} UI.update(); }})();'''
-    print(run_js)
     app.mainwin.run_js(run_js)
 app.wait()
