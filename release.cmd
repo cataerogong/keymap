97ae518f95375aac515674a000f029e934a2f406
CataeroGong <cataerogong@hotmail.com>
2024-01-09 15:36:11 +0800

    fix release.cmd

--- release.cmd
+++ release.cmd
@@ -11,15 +11,16 @@ mkdir pack_tmp
 copy /y keymap.html pack_tmp\
 copy /y README.md pack_tmp\
 copy /y LICENSE pack_tmp\
-xcopy /s exe\dist\keymap\*.* pack_tmp
+xcopy /s exe\dist\keymap\keymap.exe pack_tmp
+xcopy /s exe\dist\keymap\_internal\ pack_tmp\_internal\
 copy /y exe\README.md pack_tmp\README-keymap-exe.md
 del pack_tmp\_internal\libcrypto-1_1.dll
 rmdir /s /q pack_tmp\_internal\webui\webui-macos-clang-arm64
 rmdir /s /q pack_tmp\_internal\webui\webui-macos-clang-x64
 
 if not exist release mkdir release
-if exist release\keymap-%__version__%-html.zip del keymap-%__version__%-html.zip
-if exist release\keymap-%__version__%-exe-x64.zip del keymap-%__version__%-exe-x64.zip
+if exist release\keymap-%__version__%-html.zip del release\keymap-%__version__%-html.zip
+if exist release\keymap-%__version__%-exe-x64.zip del release\keymap-%__version__%-exe-x64.zip
 
 cd pack_tmp
 ..\zip ..\release\keymap-%__version__%-html.zip keymap.html README.md LICENSE
