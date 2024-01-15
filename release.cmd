b5d261bdac567078216376db00ec2253184fe232
CataeroGong <cataerogong@hotmail.com>
2024-01-15 12:34:11 +0800

    mod release.cmd

--- release.cmd
+++ release.cmd
@@ -5,6 +5,21 @@ echo release version: %__version__%
 echo.
 pause
 
+if not exist release mkdir release
+
+if "%~1" neq "" goto :%~1
+
+call :html
+call :exe
+
+exit /b
+
+:html
+if exist release\keymap-%__version__%-html.zip del release\keymap-%__version__%-html.zip
+zip release\keymap-%__version__%-html.zip keymap.html README.md LICENSE
+exit /b
+
+:exe
 if exist pack_tmp rmdir /s /q pack_tmp
 mkdir pack_tmp
 mkdir pack_tmp\data
@@ -16,12 +31,8 @@ xcopy /s exe\dist\keymap\keymap.exe pack_tmp
 xcopy /s exe\dist\keymap\_runtime\ pack_tmp\_runtime\
 copy /y exe\README.md pack_tmp\README-keymap-exe.md
 
-if not exist release mkdir release
-if exist release\keymap-%__version__%-html.zip del release\keymap-%__version__%-html.zip
 if exist release\keymap-%__version__%-exe-x64.zip del release\keymap-%__version__%-exe-x64.zip
-
 cd pack_tmp
-..\zip ..\release\keymap-%__version__%-html.zip keymap.html README.md LICENSE
 ..\zip -r ..\release\keymap-%__version__%-exe-x64.zip keymap.* README* LICENSE _runtime\ data\
 cd ..
 exit /b
