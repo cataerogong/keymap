b429e5f626c835e70388da8f851aea7f2755c3f3
CataeroGong <cataerogong@hotmail.com>
2024-02-28 14:20:28 +0800

    v3.4.0
    * [+] 快捷键信息文字增加“内部超链”功能

--- release.cmd
+++ release.cmd
@@ -5,56 +5,31 @@ echo release version: %__version__%
 echo.
 pause
 
-if not exist release mkdir release
-
-if "%~1" neq "" goto :%~1
-
-call :html
-call :clean
-call :exe
-call :tray
-
-exit /b
-
-:html
-if exist release\keymap-%__version__%-html.zip del release\keymap-%__version__%-html.zip
-zip release\keymap-%__version__%-html.zip keymap.html README.md MANUAL.md LICENSE js\* *.example.* keymaps\*
-exit /b
-
-:clean
 pushd exe
-call build clean
+call build
 popd
+
 if exist pack_tmp rmdir /s /q pack_tmp
-exit /b
-
-:exe
-pushd exe
-call build exe
-popd
-if not exist pack_tmp\data mkdir pack_tmp\data
-::copy /y keymap.html pack_tmp\
-::copy /y README.md pack_tmp\
-::copy /y MANUAL.md pack_tmp\
+mkdir pack_tmp\data
+copy /y keymap.html pack_tmp\
+copy /y README.md pack_tmp\
+copy /y MANUAL.md pack_tmp\
 copy /y LICENSE pack_tmp\
+copy /y *.example.* pack_tmp\
+xcopy /y /s js\ pack_tmp\js\
+xcopy /y /s keymaps\ pack_tmp\keymaps\
 copy /y exe\dist\keymap\keymap.exe pack_tmp\
 xcopy /y /s exe\dist\keymap\_runtime\ pack_tmp\_runtime\
 copy /y exe\README.md pack_tmp\README-keymap-exe.md
-if exist release\keymap-%__version__%-exe-x64.zip del release\keymap-%__version__%-exe-x64.zip
-cd pack_tmp
-zip -r ..\release\keymap-%__version__%-exe-x64.zip keymap.* README-keymap-exe.md LICENSE _runtime\ data\
-cd ..
-exit /b
-
-:tray
-pushd exe
-call build tray
-popd
-if not exist pack_tmp mkdir pack_tmp
 copy /y exe\dist\keymap\keymapTray.exe pack_tmp\
 copy /y exe\dist\keymap\keymapTray.ini pack_tmp\
-if exist release\keymap-%__version__%-tray-x64.zip del release\keymap-%__version__%-tray-x64.zip
-cd pack_tmp
-zip -r ..\release\keymap-%__version__%-tray-x64.zip keymapTray.* LICENSE
-cd ..
+
+if not exist release mkdir release
+if exist release\keymap-%__version__%-all.zip del release\keymap-%__version__%-all.zip
+if exist release\keymap-%__version__%-html.zip del release\keymap-%__version__%-html.zip
+pushd pack_tmp
+zip -r ..\release\keymap-%__version__%-html.zip keymap.html README.md MANUAL.md LICENSE *.example.* js\ keymaps\
+zip -r ..\release\keymap-%__version__%-all.zip *
+popd
+
 exit /b
